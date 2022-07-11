5a4e92de23fec8dfb06fbada07e3ae7375815ed1
DAEHYEONJO <jdhqpt12@gmail.com>
2022-07-11 20:51:57 +0900

    [Feat] Login Activity 레이아웃 1. kopubword_dotum font 추가 2. outfit font 추가

--- app/src/main/java/com/example/gitreposearch/ui/LoginActivity.kt
+++ app/src/main/java/com/example/gitreposearch/ui/LoginActivity.kt
@@ -3,10 +3,11 @@ package com.example.gitreposearch.ui
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.gitreposearch.R
-import com.example.gitreposearch.databinding.ActivityMainBinding
+import com.example.gitreposearch.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
-    private val binding: ActivityMainBinding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+
+    private val binding: ActivityLoginBinding by lazy { ActivityLoginBinding.inflate(layoutInflater) }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
