a52279148bbc85c95ec0fb1ed09b626f772538f6
DAEHYEONJO <jdhqpt12@gmail.com>
2022-07-12 17:07:14 +0900

    [Chore] github base url 수정

--- app/src/main/java/com/example/gitreposearch/network/GithubApi.kt
+++ app/src/main/java/com/example/gitreposearch/network/GithubApi.kt
@@ -4,10 +4,7 @@ import com.example.gitreposearch.BuildConfig
 import com.example.gitreposearch.data.Token
 import com.example.gitreposearch.utils.Constants
 import retrofit2.Response
-import retrofit2.http.Field
-import retrofit2.http.FormUrlEncoded
-import retrofit2.http.Headers
-import retrofit2.http.POST
+import retrofit2.http.*
 
 interface GithubApi {
 
--- app/src/main/java/com/example/gitreposearch/utils/Constants.kt
+++ app/src/main/java/com/example/gitreposearch/utils/Constants.kt
@@ -4,6 +4,6 @@ object Constants {
 
     const val oauthLoginUrl = "https://github.com/login/oauth/authorize"
     const val githubDomainUrl = "https://github.com/"
-    const val githubBaseUrl = "https://github.com/"
+    const val githubBaseUrl = "https://api.github.com/"
 
 }
