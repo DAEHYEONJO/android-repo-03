8988fcc9669d0b7e5435f18a0ab2b04e8d7c575b
DAEHYEONJO <jdhqpt12@gmail.com>
2022-07-16 03:31:44 +0900

    [Feat] Paging debuging을 위한okHttpClient Logger 추가

--- app/src/main/java/com/example/gitreposearch/network/GithubApiImpl.kt
+++ app/src/main/java/com/example/gitreposearch/network/GithubApiImpl.kt
@@ -1,13 +1,26 @@
 package com.example.gitreposearch.network
 
+import com.example.gitreposearch.BuildConfig
 import com.example.gitreposearch.utils.Constants
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object GithubApiImpl {
 
+    private val okHttpClient = OkHttpClient.Builder()
+        .addInterceptor(HttpLoggingInterceptor().apply {
+            level = if (BuildConfig.DEBUG){
+                HttpLoggingInterceptor.Level.BODY
+            }else{
+                HttpLoggingInterceptor.Level.NONE
+            }
+        }).build()
+
     private val retrofit = Retrofit.Builder()
         .baseUrl(Constants.githubBaseUrl)
+        .client(okHttpClient)
         .addConverterFactory(GsonConverterFactory.create())
         .build()
 
